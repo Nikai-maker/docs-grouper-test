88ebff86dad7c9e3a7631901f0c2ab0f241d7b22
Nikai-maker <superkarnauhov@mail.ru>
2026-07-30 14:22:33 +0300

    Рефакторинг

--- app/core/database.py
+++ app/core/database.py
@@ -1,3 +1,5 @@
+from collections.abc import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -17,6 +19,6 @@ async def init_db() -> None:
         await conn.run_sync(Base.metadata.create_all)
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
